5b36d4be96bdddcb3d680c2a3a6484bfedea0f2e
Vivek Kumar <92210976+vivekkumar004@users.noreply.github.com>
2022-04-24 20:42:24 +0530

    Add files via upload

--- React-App/src/hooks/usePersistence.ts
+++ React-App/src/hooks/usePersistence.ts
@@ -1,1 +1,18 @@
+import { useState, useEffect } from "react";
 
+function usePersistence(
+  localStorageKey: string,
+  initValue: string
+): [string, (e: string) => void] {
+  const [value, setValue] = useState(
+    localStorage.getItem(localStorageKey) ?? initValue
+  );
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value]);
+
+  return [value, setValue];
+}
+
+export default usePersistence;
