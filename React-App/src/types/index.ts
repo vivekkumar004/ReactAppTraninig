fb3df95b71d2d15541b8ada44fa2bf14938fbb82
Vivek Kumar <92210976+vivekkumar004@users.noreply.github.com>
2022-04-26 18:56:06 +0530

    Add files via upload

--- React-App/src/types/index.ts
+++ React-App/src/types/index.ts
@@ -1,9 +1,39 @@
 export type StoryType = {
-    title: string;
-    url: string;
-    created_at: string;
-    author: string;
-    points: number;
-    num_comments: number;
-    objectID: number;
-  };
+  title: string;
+  url: string;
+  created_at: string;
+  author: string;
+  points: number;
+  num_comments: number;
+  objectID: number;
+};
+
+export type StateType = {
+  data: Array<StoryType>;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+interface ISetStories {
+  type: "SET_STORIES";
+  payload: { data: Array<StoryType> };
+}
+
+interface IInitFetch {
+  type: "INIT_FETCH";
+}
+
+interface IFetchFailure {
+  type: "FETCH_FAILURE";
+}
+
+interface IRemoveStory {
+  type: "REMOVE_STORY";
+  payload: { id: number };
+}
+
+export type ActionType =
+  | ISetStories
+  | IInitFetch
+  | IFetchFailure
+  | IRemoveStory;
